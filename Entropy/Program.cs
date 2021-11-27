bc9d6ff512cfbd13ab1ccf4010babe02e6395847
Nicholas Finefter <nfinefter@gmail.com>
2021-11-26 19:42:20 -0800

    Add project files.

---
+++ Entropy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Entropy
+{
+    class Program
+    {
+        public static string Compressor(string input)
+        {
+            string compressedString = "";
+
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int unique = 0;
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (input[i] == input[j])
+                    {
+                        unique++;
+                    }
+                    compressedString += (input[i] + unique);
+                }
+            }
+            return compressedString;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a string!");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(Compressor(input));
+
+        }
+    }
+}
